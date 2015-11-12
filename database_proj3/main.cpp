6a9973b9eaca887eb3b003b9b43856786b9dc08a
huanshi.hy <huanshi.hy@alibaba-inc.com>
2015-11-11 21:35:49 -0600

    Initial Commit

---
+++ database_proj3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  database_proj3
+//
+//  Created by Hongyg on 15/11/11.
+//  Copyright (c) 2015年 Hongyg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
