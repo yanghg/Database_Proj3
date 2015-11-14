9bac416dad93a0a148a6a464999e8ba219019e91
huanshi.hy <huanshi.hy@alibaba-inc.com>
2015-11-13 23:09:25 -0600

    init

--- database_proj3/main.cpp
+++ database_proj3/main.cpp
@@ -6,10 +6,36 @@
 //  Copyright (c) 2015年 Hongyg. All rights reserved.
 //
 
+#include <mysql.h>
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    login();
     return 0;
 }
+
+int login() {
+    // insert code here...
+    MYSQL *conn;
+    MYSQL_RES *res_set;
+    MYSQL_ROW row;
+    conn = mysql_init(NULL);
+    mysql_real_connect(conn, "localhost", "root", "fangxiao", "project3-nudb", 3307, 0, 0);
+    
+    
+    // login
+    string username = "Harry Jenkins";
+    string password = "butterflY";
+    string callProcedure = "CALL login('"+ username + "', '" + password + "', @isValid);";
+    mysql_query(conn, callProcedure.c_str());
+    mysql_query(conn, "SELECT @isValid");
+    
+    
+    res_set = mysql_use_result(conn);
+    row = mysql_fetch_row( res_set );
+    cout << row[0] << endl;
+    return 0;
+}
+
+
