48595e048530492ea97e8c9d62bf68f46deddfee
missfxhr <xiaofang2016@u.northwestern.edu>
2015-11-14 19:16:19 -0600

    added workflow, deleted login

--- database_proj3/workflow.cpp
+++ database_proj3/workflow.cpp
@@ -7,3 +7,32 @@
 //
 
 #include "workflow.h"
+#include "mysql.h"
+#include <iostream>
+using namespace std;
+
+int WorkFLow::login(MYSQL *conn){
+    // insert code here...
+
+    MYSQL_RES *res_set;
+    MYSQL_ROW row;
+
+    // input username and password
+    string username = "";
+    string password = "";
+    cout << "Username:" << endl;
+    getline(cin, username, '\n');
+    cout << "Password:" << endl;
+    cin >> password;
+
+    // check infomation
+    string callProcedure = "CALL login('"+ username + "', '" + password + "', @isValid);";
+    mysql_query(conn, callProcedure.c_str());
+    mysql_query(conn, "SELECT @isValid");
+
+
+    res_set = mysql_use_result(conn);
+    row = mysql_fetch_row(res_set);
+    return atoi(row[0]);
+}
+
--- database_proj3/workflow.h
+++ database_proj3/workflow.h
@@ -1,14 +1,23 @@
 //
-//  workflow.hpp
+//  workflow.h
 //  database_proj3
 //
 //  Created by FangXiao on 11/14/15.
 //  Copyright © 2015 Hongyg. All rights reserved.
 //
 
-#ifndef workflow_hpp
-#define workflow_hpp
+#ifndef workflow_h
+#define workflow_h
 
 #include <stdio.h>
+
+#include "student.h"
+
+class WorkFLow{
+public:
+    int login();
+private:
+    Student currentStudent;
+};
 
 #endif /* workflow_hpp */
