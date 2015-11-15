05dc2b469f7b49981d47384040892381c21577b4
missfxhr <xiaofang2016@u.northwestern.edu>
2015-11-14 18:11:14 -0600

    transfered to clion

---
+++ database_proj3/workflow.cpp
@@ -0,0 +1,9 @@
+//
+//  workflow.cpp
+//  database_proj3
+//
+//  Created by FangXiao on 11/14/15.
+//  Copyright © 2015 Hongyg. All rights reserved.
+//
+
+#include "workflow.h"
---
+++ database_proj3/workflow.h
@@ -0,0 +1,14 @@
+//
+//  workflow.hpp
+//  database_proj3
+//
+//  Created by FangXiao on 11/14/15.
+//  Copyright © 2015 Hongyg. All rights reserved.
+//
+
+#ifndef workflow_hpp
+#define workflow_hpp
+
+#include <stdio.h>
+
+#endif /* workflow_hpp */
